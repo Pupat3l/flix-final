e6d2bdf2189e3263a9f75baafd7d3d7a27f909e4
Pupat3l <pujan.patel2002@gmail.com>
2022-09-18 14:41:31 -0400

    Added Assignment 1 for IOS s2

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'flix_p1' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for flix_p1
+  pod "AlamofireImage"
+
+
+  target 'flix_p1Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'flix_p1UITests' do
+    # Pods for testing
+  end
+
+end
---
+++ flix_p1UITests/flix_p1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  flix_p1UITestsLaunchTests.swift
+//  flix_p1UITests
+//
+//  Created by Pujan Patel on 9/18/22.
+//
+
+import XCTest
+
+final class flix_p1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
