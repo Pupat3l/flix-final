6a2f9bae6eb943dd775ef4abefdf95eec97354d0
Pupat3l <pujan.patel2002@gmail.com>
2022-09-23 16:16:56 -0400

    Flixpart2

---
+++ flix_p1/movieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flix_p1
+//
+//  Created by Pujan Patel on 9/23/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
