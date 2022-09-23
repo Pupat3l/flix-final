8c2ce8446683a1f66ddf62de7d8e273895cd4a27
Pupat3l <pujan.patel2002@gmail.com>
2022-09-23 16:44:59 -0400

    updated_

--- flix_p1/MovieGridViewController.swift
+++ flix_p1/MovieGridViewController.swift
@@ -21,13 +21,13 @@ class MovieGridViewController: UIViewController, UICollectionViewDelegate, UICol
         collectionView.dataSource = self
         
         let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        layout.minimumLineSpacing = 4
-        layout.minimumInteritemSpacing = 4
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
         
-        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing) / 3
         layout.itemSize = CGSize(width: width, height: (width * 3 / 2))
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/634649/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/363088/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
